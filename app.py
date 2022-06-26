f0e84b047a0ae3b01147c3ba22d1de567a3f4859
Macpresto <mcmpresto25@gmail.com>
2022-06-26 15:27:18 +0800

    fix caching of static files fixes as bug where the produced animation is not getting updated since the browser is caching the old animation

--- app.py
+++ app.py
@@ -4,10 +4,32 @@ import time
 from movie import processVideo
 from clean import cleanText, processText
 from sentiment import translate
+import os
 
 app = Flask(__name__)
 
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+@app.context_processor
+def override_url_for():
+    """
+    Generate a new token on every request to prevent the browser from
+    caching static files.
+    """
+    return dict(url_for=dated_url_for)
+
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
+
+
 @app.route("/", methods=["POST", "GET"])
 
 def home():
